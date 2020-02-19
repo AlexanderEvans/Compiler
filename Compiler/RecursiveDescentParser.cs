cecd3daea9528eb11ff0ae9747db1adccb5bd301
AlexanderEvans <AlexanderEvans@users.noreply.github.com>
2020-02-19 03:46:01 -0600

    Create RDP

---
+++ Compiler/RecursiveDescentParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Compiler
+{
+    class RecursiveDescentParser
+    {
+        static Scanner scanner=null;
+        public static void Parse(Scanner scannerObj)
+        {
+            if (scannerObj == null)
+                Printer.ErrLine("Scanner object must be passed to RecursiveDescentParser!");
+            else
+                scanner = scannerObj;
+        }
+
+    }
+}
